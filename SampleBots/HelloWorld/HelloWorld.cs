d53da89b7b76fd572404787feb3330e2f8a9ba84
TimothyJi <Timothyji111@gmail.com>
2018-03-11 19:31:34 -0400

    Initial Commit

---
+++ SampleBots/HelloWorld/HelloWorld.cs
@@ -0,0 +1,40 @@
+﻿using EEBLAPI;
+using EEBLAPI.Chat;
+using EEBLAPI.Event;
+
+namespace HelloWorld
+{
+    public class HelloWorld : Bot
+    {
+        public override string NAME => "HelloWorld";
+
+        public override string VERSION => "1.0.0";
+
+        public override string AUTHOR => "Timothyji";
+
+        IChatQueue chat;
+
+        public override bool PreLoad(EEBLPreLoadEvent e)
+        {
+            Logger = e.GetLogger();
+
+            return base.PreLoad(e);
+        }
+
+        public override bool Load(EEBLLoadEvent e)
+        {
+            Logger.Info("Hello World!");
+
+            return base.Load(e);
+        }
+
+        public override bool OnInit(EEBLInitEvent e)
+        {
+            chat = e.GetChatQueue();
+
+            chat.SendChat(ChatPriority.NORMAL, "Hello World!");
+
+            return base.OnInit(e);
+        }
+    }
+}
---
+++ SampleBots/HiPlayer/HiPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using EEBLAPI;
+using EEBLAPI.Chat;
+using EEBLAPI.Event;
+using EEBLAPI.Player.Utils;
+
+namespace HiPlayer
+{
+    public class HiPlayer : Bot
+    {
+        public override string NAME => "HiPlayer";
+        public override string VERSION => "1.0.0";
+        public override string AUTHOR => "Timothyji";
+
+        IChatQueue chat;
+        IPlayerHandler playerHandler;
+
+        //recommended so the bot can log information to console.
+        public override bool PreLoad(EEBLPreLoadEvent e)
+        {
+            Logger = e.GetLogger();
+            return base.PreLoad(e);
+        }
+
+        public override bool OnPreInit(EEBLPreInitEvent e)
+        {
+            playerHandler = e.GetPlayerHandler();
+            playerHandler.OnPlayerJoin_Subscribe(HiPlayer_OnPlayerJoin);
+            return base.OnPreInit(e);
+        }
+
+        private void HiPlayer_OnPlayerJoin(EEBLAPI.Player.IPlayer player, EEBLAPI.Message.IMessageAdd message)
+        {
+            chat.SendChat(ChatPriority.NORMAL, "Hello " + player.Username);
+        }
+
+        public override bool OnInit(EEBLInitEvent e)
+        {
+            chat = e.GetChatQueue();
+            return base.OnInit(e);
+        }
+    }
+}
